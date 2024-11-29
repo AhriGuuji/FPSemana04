22b2aa690b005206195bc782e93b62458c03ca06
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2024-11-29 11:19:53 +0000

    completed

--- read_json.py
+++ read_json.py
@@ -1,7 +1,10 @@
 import json
-
-file = open("data.json")
-data= json.load(file)
-print(data)
-
-final.close()
+file=input()
+try:
+    with open(file,encoding='utf8') as data_file:
+        data = json.load(data_file)
+    print(data)
+except:
+    print("Ocorreu um erro!")
+finally:
+    print("Processo concluído!")
